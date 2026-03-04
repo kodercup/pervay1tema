205603656fa513cb691d346c214e1c91fa21124f
Марс <@DESKTOP-89D97PP>
2026-03-05 02:18:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static double Calculate(double x)
+    {
+        return 7 * x * x * 3 * x + 6;
+    }
+
+    static double Calc(double a)
+    {
+        return 12 * a * a + 7 * a + 12;
+
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Программа вычисления функции y = 7x^2 + 3 x + 6 ");
+        Console.WriteLine("Программа вычисления y = 12a^2 + 7a + 12 ");
+
+        Console.WriteLine("Введите значение x: ");
+
+        Console.WriteLine("Введите значение а : ");
+
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        double result = Calculate(x);
+        double res = Calc(a);
+
+        Console.WriteLine($"Результат: {result}");
+        Console.WriteLine($"Результат {res}");
+
+    }
+
+}
